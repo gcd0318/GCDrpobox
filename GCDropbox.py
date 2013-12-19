86a00f16ed47116891773dea76fe2febb0194ccf
gcd0318 <gcd0318@hotmail.com>
2013-12-20 03:41:39 +0800

    add parser for metadata

--- GCDropbox.py
+++ GCDropbox.py
@@ -7,18 +7,32 @@ class GCDropbox(DropboxClient):
     def __init__(self, app_key, app_secret, locale='UTF-8'):
         oauth_flow = DropboxOAuth2FlowNoRedirect(app_key, app_secret)
         authorize_url = oauth_flow.start()
-        print("1, Go to: " + authorize_url + "\n",\
-              "2, Click \"Allow\" (log in if necessary)\n",\
-              "3, Copy the authorization code\n")
-        access_token, user_id = oauth_flow.finish(input("Enter the authorization code here:").strip())
+        print(authorize_url)
+        access_token, user_id = oauth_flow.finish(input("code: ").strip())
+        print(access_token)
         DropboxClient.__init__(self, access_token, locale)
     def upload(self, fp):
         pass
     def get_list(self, path):
         pass
 
+def parse(client, root='/'):
+    cont = client.metadata(root)['contents']
+    print(root, len(cont))
+    res = []
+    for item in cont:
+        if(item['is_dir']):
+            res = res + parse(client, item['path'])
+        else:
+            res.append(item)
+    return res
+
 if __name__ == "__main__":
-    from config import *
-    gcd = GCDropbox(app_key, app_secret)
-    metadata = gcd.metadata('/dt2')
-    print(metadata)
+    save_path = '/mnt/share/dropbox/'
+    app_key = '1lso43s8iv08kv2'
+    app_secret = 'pnt9p61hg42vpwq'
+    access_token = None
+
+    for i in parse(GCDropbox(app_key, app_secret)):
+        print(i['path'], i['bytes'])
+
--- config.py
+++
@@ -1,4 +0,0 @@
-save_path = '/mnt/share/dropbox/'
-app_key = '1lso43s8iv08kv2'
-app_secret = 'pnt9p61hg42vpwq'
-access_token = None
